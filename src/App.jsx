d53d66ba82e39872076e1fcb6c46949882ea34be
ErikSanzana <ersanzana@gmail.com>
2023-08-23 10:25:57 -0400

    se agrega boostrap a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './App.css'
 import Registro from './components/Registro'
 import Alert from './components/Alert'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 

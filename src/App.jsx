58f73c1ff0947e0d4a9f0ca6a5bfc175d587a498
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-08-23 22:24:04 -0400

    Mañana sigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import Registro from './components/Registro';
-import Alert from './components/Alert';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [mensaje, setMensaje] = useState("");
-
   return (
     <>
-      <Registro setMensaje={setMensaje} /> 
-      {mensaje && <Alert message={mensaje} />} 
+      <Registro /> 
     </>
   );
 }
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -7,28 +7,22 @@ import Alert from './Alert';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
-const Registro = ({ setMensaje }) => {
-  
-  const [alert, setAlert] = useState({
-    error: "",
-    msg: "",
-    color: ""
-  })
-
+const Registro = () => {
+  const [] = useState("");
 
   return (
     <>
       <h1 className='mb-3'>Crea una cuenta</h1>
 
-      <SocialButton setMensaje={setMensaje}
+      <SocialButton
         face={faFacebookF}
         git={faGithub}
         linked={faLinkedinIn}
       />
 
-      <Formulario
-        setAlert={setAlert}
-      /> 
+      <Formulario /> 
+
+      <Alert />
     </>
   );
 }
